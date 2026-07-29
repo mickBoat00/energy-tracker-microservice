36b04599f03a145fdbb6e1bd5d901b7f56b1a09c
mickBoat00 <m.boateng0000@gmail.com>
2026-07-29 23:21:12 +0000

    add seed data to device and user services

---
+++ user-service/src/main/java/com/meichel/user_service/seed/UserSeeder.java
@@ -0,0 +1,66 @@
+package com.meichel.user_service.seed;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.meichel.user_service.entity.User;
+import com.meichel.user_service.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class UserSeeder implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public UserSeeder(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (userRepository.count() > 0) {
+            log.info("Users already seeded, skipping.");
+            return;
+        }
+
+        userRepository.save(User.builder()
+                .firstName("Kwame")
+                .lastName("Mensah")
+                .address("12 Ring Road, Accra")
+                .email("kwame@example.com")
+                .enableAlerting(true)
+                .alertingThreshold(5)
+                .build());
+
+        userRepository.save(User.builder()
+                .firstName("Ama")
+                .lastName("Owusu")
+                .address("5 Oxford St, Osu")
+                .email("ama@example.com")
+                .enableAlerting(false)
+                .alertingThreshold(0)
+                .build());
+
+        userRepository.save(User.builder()
+                .firstName("Kofi")
+                .lastName("Boateng")
+                .address("22 Spintex Road, Accra")
+                .email("kofi@example.com")
+                .enableAlerting(true)
+                .alertingThreshold(10)
+                .build());
+
+        userRepository.save(User.builder()
+                .firstName("Abena")
+                .lastName("Asante")
+                .address("9 Cantonments Rd, Accra")
+                .email("abena@example.com")
+                .enableAlerting(true)
+                .alertingThreshold(3)
+                .build());
+
+        log.info("Seeded {} users.", userRepository.count());
+    }
+}
